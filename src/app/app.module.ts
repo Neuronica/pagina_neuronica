4dc974efb6d8e44ee17b16c976152e3cf0ea1d34
Neuronica <gerencia@neuronica.co>
2023-09-16 13:11:39 -0500

    Inicio de menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NmenuComponent } from './nmenu/nmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NmenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nmenu/nmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nmenu',
+  templateUrl: './nmenu.component.html',
+  styleUrls: ['./nmenu.component.css']
+})
+export class NmenuComponent {
+
+}
