96e2e02136f114f8cb36b95e333495ba9a3a7371
Neuronica <gerencia@neuronica.co>
2023-09-20 10:49:35 -0500

    Funcionamiento de animaciones pagina Inicio

--- src/app/nmenu/nmenu.component.ts
+++ src/app/nmenu/nmenu.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-nmenu',
   templateUrl: './nmenu.component.html',
   styleUrls: ['./nmenu.component.css']
 })
-export class NmenuComponent {
+export class NmenuComponent{
+
+  ngOnInit(){
+    AOS.init()
+    window.addEventListener('load', AOS.refresh)
+
+  }
 
 }
