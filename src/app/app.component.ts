3082a17e38a5b9d4b057fcf1c3321671bf9667b0
Neuronica <gerencia@neuronica.co>
2023-09-25 16:31:53 -0500

    Optimización inicio para celular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'neuronica';
-  
+  public getwidth: any;
+  public getheight: any;
+  public menuCel: boolean = false;
+  abrirMenu(){
+    this.menuCel =! this.menuCel;
+  }
+
+  ngOnInit(){
+    this.getwidth = window.innerWidth;
+    this.getheight = window.innerHeight;
+  }
+
+  @HostListener('window:resize', ['$event'])
+
+  onWindowResize(){
+    this.getwidth = window.innerWidth;
+    this.getheight = window.innerHeight;
+  }
 }
+
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import * as AOS from 'aos';
 
 @Component({
@@ -7,8 +7,20 @@ import * as AOS from 'aos';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+
+  public getwidth: any;
+  public getheight: any;
   ngOnInit(){
     AOS.init();
     window.addEventListener('load', AOS.refresh);
+    this.getwidth = window.innerWidth;
+    this.getheight = window.innerHeight
+  }
+
+  @HostListener('window:resize', ['$event'])
+
+  onWindowResize(){
+    this.getwidth = window.innerWidth;
+    this.getheight = window.innerHeight;
   }
 }
