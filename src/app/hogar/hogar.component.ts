e925e6a8fe035583b1bbe432de521defd67b34ce
Neuronica <gerencia@neuronica.co>
2023-09-28 17:17:24 -0500

    Optimicacion móvil hogar

--- src/app/hogar/hogar.component.ts
+++ src/app/hogar/hogar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 import * as AOS from 'aos';
 
 @Component({
@@ -7,8 +7,19 @@ import * as AOS from 'aos';
   styleUrls: ['./hogar.component.css']
 })
 export class HogarComponent {
+  public getwidth: any;
+  public getheight: any;
   ngOnInit(){
     AOS.init();
     window.addEventListener('load', AOS.refresh);
+    this.getwidth = window.innerWidth;
+    this.getheight = window.innerHeight
+  }
+
+  @HostListener('window:resize', ['$event'])
+
+  onWindowResize(){
+    this.getwidth = window.innerWidth;
+    this.getheight = window.innerHeight;
   }
 }
