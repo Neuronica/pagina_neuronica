b4e723eaa0a9de02bf2fd1beb224c931d6c11233
Neuronica <gerencia@neuronica.co>
2024-01-11 11:37:17 -0500

    Solución problemas para deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,17 +6,3 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-import * as functions from 'firebase-functions';
-
-
-export const generateSitemap = functions.https.onRequest((req, res) => {
-    const inicio = fetch('https://www.neuronica.co/');
-
-    let sitemap = '';
-
-    Promise.all([
-        inicio.then(result =>result.json)
-    ])
-
-    .then(jsonData =>)
-});
