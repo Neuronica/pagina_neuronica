02483fd3562f3291013fca5222ee9a7585e49c31
sfmerchan <sfmerchans@neuronica.co>
2024-08-26 08:46:27 -0500

    Fin Cambios Página de inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,10 @@ export class AppComponent implements OnInit{
   public getheight: any;
   public menuCel: boolean = false;
   public iconoVariable: boolean = false;
+  public openMenu: boolean = false;
+  public desplegar: boolean = false;
 
-  isBrowser: boolean;
+  public isBrowser: boolean;
 
   constructor( @Inject(PLATFORM_ID) platformId: Object) {
     this.isBrowser = isPlatformBrowser(platformId);
@@ -24,9 +26,22 @@ export class AppComponent implements OnInit{
     if (this.isBrowser){
       this.menuCel =! this.menuCel;
       this.iconoVariable =! this.iconoVariable;
+      this.openMenu =! this.openMenu;
+      this.desplegar  = false;
     }
   }
 
+  desplegarMenu(){
+    if(this.isBrowser){
+      this.desplegar =! this.desplegar;
+    }
+  }
+
+  cerrarVariables(){
+    if(this.desplegar){
+      this.desplegar = false;
+    }
+  }
 
   ngOnInit(){
     if (this.isBrowser){
